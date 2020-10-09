fc41abcf972f0889fad3610de63a64b67304da36
Hermen Duipmans <hermenduipmans@gmail.com>
2020-10-09 14:54:39 +0200

    added support for formatter functions

--- dev/main.js
+++ dev/main.js
@@ -1,35 +1,65 @@
 import Vue from 'vue';
 import minimalTable from '../dist/index';
+import BootstrapVue from 'bootstrap-vue';
+Vue.use(BootstrapVue);
 
 const dec2hex = dec => (dec < 10 ? '0' + String(dec) : dec.toString(16));
 const generateRandomString = len => Array.from(crypto.getRandomValues(new Uint8Array(len)), dec2hex).join('');
 
 new Vue({
     data: {
-        numberOfRows: 20,
-        numberOfFields: 3,
+        valueField: '',
+        numberOfRows: 100,
+        numberOfFields: 5,
+        formatter: '',
         useFormatterFunction: true,
         fields: [],
         items: [],
+        field: {},
+        item: {},
     },
     methods: {
         createData() {
-            for (let j = 0; j < this.numberOfFields; j++) {
-                this.fields.push(generateRandomString(5));
+            if (this.fields.length < this.numberOfFields) {
+                for (let j = 0; j < this.numberOfFields; j++) {
+                    let field = {id: j, label: generateRandomString(5)};
+                    this.fields.push(field);
+                }
+                if (this.useFormatterFunction) {
+                    this.fields.push('formatter');
+                }
             }
-            for (let i = 0; i < this.numberOfRows; i++) {
+            for (let i = this.items.length; i < this.numberOfRows; i++) {
                 let item = {};
                 for (let field of this.fields) {
+                    if (field == 'formatter') {
+                        item[field] = () => {
+                            if (this.numberOfRows % 2 == 0) {
+                                return 'Even!';
+                            }
+                            return 'Oneven!';
+                        };
+                        break;
+                    }
                     item[field] = generateRandomString(10);
                 }
                 this.items.push(item);
             }
+        },
+        addRow() {
+            console.time('addRow');
+            this.numberOfRows++;
+            this.createData();
+            console.timeEnd('addRow');
         },
     },
     mounted() {
         this.createData();
     },
     render(h) {
-        return h(minimalTable, {props: {fields: this.fields, items: this.items}});
+        const table = h(minimalTable, {props: {fields: this.fields, items: this.items}});
+        const btable = h('b-table', {props: {fields: this.fields, items: this.items}});
+        const addButton = h('button', {on: {click: this.addRow}}, 'Voeg een rij toe');
+        return h('div', [addButton, table, btable]);
     },
 }).$mount('#app');
--- dist/index.js
+++ dist/index.js
@@ -15,26 +15,45 @@ var index = {
             type: Array,
             required: true,
         },
+        valueField: {
+            type: String,
+            required: false,
+            default: () => 'label',
+        },
+        perPage: {
+            type: Number,
+            required: false,
+            default: () => 20,
+        },
     },
     render(h) {
         const tableheader = h('thead', [
             h('tr', [
                 this.fields.map(field => {
-                    return h('td', [field]);
+                    return h('td', [field[this.valueField]]);
                 }),
             ]),
         ]);
-
         const tableRows = this.items.map(item => {
-            let cells = this.fields.map(field => {
-                return h('td', [item[field]]);
-            });
-            return h('tr', [cells]);
+            console.log(this.perPage);
+            for (let i = 0; i < this.perPage; i++) {
+                let cells = this.fields.map(field => {
+                    if (field == 'formatter') {
+                        if (!item['result']) {
+                            item['result'] = item[field].call();
+                            return h('td', [item['result']]);
+                        }
+                        return h('td', [item['result']]);
+                    }
+                    return h('td', [item[field]]);
+                });
+                return h('tr', [cells]);
+            }
         });
 
         const tableBody = h('tbody', [tableRows]);
-
-        return h('table', [[tableheader], [tableBody]]);
+        const minimalTable = h('table', [[tableheader], [tableBody]]);
+        return h('div', [minimalTable]);
     },
 };
 
