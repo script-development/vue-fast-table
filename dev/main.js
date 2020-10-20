6a9934c0e2f672fc6bac62215756f727d52234ba
Hermen Duipmans <hermenduipmans@gmail.com>
2020-10-20 11:50:10 +0200

    changed the way test data is generated

--- dev/main.js
+++ dev/main.js
@@ -5,57 +5,57 @@ Vue.use(BootstrapVue);
 
 const dec2hex = dec => (dec < 10 ? '0' + String(dec) : dec.toString(16));
 const generateRandomString = len => Array.from(crypto.getRandomValues(new Uint8Array(len)), dec2hex).join('');
+const randomInteger = Math.floor(Math.random() * 10000);
 
 new Vue({
     data: {
         valueField: '',
         numberOfRows: 10,
-        numberOfColumns: 5,
+        numberOfFields: 3,
         formatter: '',
         useFormatterFunction: true,
         useActions: true,
-        columns: [],
+        fields: [],
         items: [],
         column: {},
         item: {},
     },
     methods: {
         createData() {
-            if (this.columns.length < this.numberOfColumns) {
-                for (let j = 0; j < this.numberOfColumns; j++) {
-                    let column = {id: j, label: generateRandomString(5)};
-                    this.columns.push(column);
+            if (this.fields.length < this.numberOfFields) {
+                for (let j = 0; j < this.numberOfFields; j++) {
+                    let field = {
+                        id: j,
+                        key: generateRandomString(5),
+                        label: generateRandomString(5),
+                    };
+                    this.fields.push(field);
                 }
-                if (this.useFormatterFunction && !this.columns.includes('formatter')) {
-                    this.columns.push('formatter');
-                }
-                if (this.useActions && !this.columns.includes('actions')) {
-                    this.columns.push('actions');
+
+                if (this.useFormatterFunction && !this.fields.includes('formatter')) {
+                    this.fields.push({
+                        id: randomInteger,
+                        key: 'formatter',
+                        formatter: () => {
+                            return this.numberOfRows % 2 == 0 ? 'even' : 'oneven';
+                        },
+                    });
                 }
             }
 
-            for (let i = this.items.length; i < this.numberOfRows; i++) {
+            for (let i = 0; i < this.numberOfRows; i++) {
                 let item = {};
-                for (let column of this.columns) {
-                    if (column == 'formatter') {
-                        item[column] = () => {
-                            if (this.numberOfRows % 2 == 0) {
-                                return 'Even!';
-                            }
-                            return 'Oneven!';
-                        };
-                    }
-                    if (column == 'actions') {
-                        item[column] = '<template><div><span>Hallo!</span></div></template>';
-                    }
-                    item[column.label] = generateRandomString(10);
+                for (const field of this.fields) {
+                    item[field.key] = generateRandomString(5);
                 }
                 this.items.push(item);
             }
         },
         addRow() {
+            console.time('adding data');
             this.numberOfRows += 1000;
             this.createData();
+            console.timeEnd('adding data');
         },
     },
     mounted() {
@@ -63,10 +63,10 @@ new Vue({
     },
     render(h) {
         // TODO :: test with all possibilities
-        const table = h(minimalTable, {props: {fields: this.columns, items: this.items}}, [h('template', 'hallo!')]);
-        // const btable = h('b-table', {props: {columns: this.columns, items: this.items}});
+        const table = h(minimalTable, {props: {fields: this.fields, items: this.items}});
+        const btable = h('b-table', {props: {fields: this.fields, items: this.items}});
         const addButton = h('button', {on: {click: this.addRow}}, 'Voeg 1000 rijen toe');
-        return h('div', [addButton, table]);
-        // return h('div', [addButton, table, btable]);
+        // return h('div', [addButton, table]);
+        return h('div', [addButton, table, btable]);
     },
 }).$mount('#app');
