63e8c061460295fb22eebd94ac8eb3e614882c65
Hermen Duipmans <hermenduipmans@gmail.com>
2020-10-26 10:54:38 +0100

    fixed some todo's

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 /**
  * @typedef {import('vue').CreateElement} CreateElement
  *
- * @todo :: id is not used in a field, check in the code below what it actually can use
  * @typedef {object} Field
- * @property {Integer} id
  * @property {String} key the string used to access an item's value
  * @property {String} label the string visible in the table
  *
@@ -38,27 +36,34 @@ export default {
         const items = props.items;
         const fields = props.fields;
 
-        // console.log(items);
-        // console.log('fields:', fields);
-
         const tableheader = h('thead', [
             h('tr', [
                 fields.map(field => {
-                    // TODO :: should show label when label is defined
-                    return h('th', {attrs: {scope: 'col', class: 'header'}}, [h('div', [field.key])]);
+                    let fieldContainsLabel = Object.prototype.hasOwnProperty.call(field, 'label');
+                    return h('th', {attrs: {scope: 'col', class: 'header'}}, [
+                        h('div', [fieldContainsLabel ? field.label : field.key]),
+                    ]);
                 }),
             ]),
         ]);
-        const tableRows = items.map((item, rowNumber) => {
+        const tableRows = items.map(item => {
             const cells = fields.map(field => {
                 if (field.tdClass) {
                     return h('td', {attrs: {class: field.tdClass(item[field.key], field.key, item)}});
                 }
 
                 if (field.formatter) {
-                    return h('td', {on: {click: () => this.clickRow(rowNumber)}}, [
-                        field.formatter(item[field.key], field.key, item),
-                    ]);
+                    return h(
+                        'td',
+                        {
+                            on: {
+                                click: () => {
+                                    if (listeners['row-clicked']) listeners['row-clicked'](item);
+                                },
+                            },
+                        },
+                        [field.formatter(item[field.key], field.key, item)]
+                    );
                 }
 
                 if (scopedSlots[`cell(${field.key})`]) {
